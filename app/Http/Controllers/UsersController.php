23e1582c5d8e7f508ed1683f93bfaec7b426ad24
jwooss <wodn4131@gmail.com>
2019-01-02 23:56:09 +0900

    Refactor: 네이티브 로그인 처리 수정

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class UsersController extends Controller
 {
@@ -17,6 +18,36 @@ class UsersController extends Controller
     }
 
     public function store(Request $request)
+    {
+        $socialUser = \App\User::whereEmail($request->input('email'))
+            ->whereNull('password')->first();
+
+        if ($socialUser) {
+            return $this->updateSocialAccount($request, $socialUser);
+        }
+
+        return $this->createNativeAccount($request);
+    }
+
+    protected function updateSocialAccount(Request $request, \App\User $user)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user->update([
+            'name' => $request->input('name'),
+            'passworkd' => bcrypt($request->input('password')),
+        ]);
+
+        auth()->login($user);
+
+        return $this->responseCreated($user->name . '님 환영합니다.');
+    }
+
+    protected function createNativeAccount(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|max:255',
